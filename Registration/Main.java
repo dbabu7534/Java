979f5d2ae04549bd8fa1e4ff059dae2d84906687
dbabu7534 <130046374+dbabu7534@users.noreply.github.com>
2023-11-14 17:11:58 +0530

    Create Main.java

---
+++ Registration/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+import practise.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("********* Welcome the Page *********");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your Option:");
+        int option = scanner.nextInt();
+
+        switch(option) {
+            case 1:
+                Registration register = new Registration();
+                break;
+        }
+
+
+    }
+}
